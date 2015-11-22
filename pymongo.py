0e128963798f146e4eae1d17d8d7502fe49449b1
Neeraj Kumar <nekumar@mix.wvu.edu>
2015-11-22 15:59:37 -0500

    Update pymongo.py

--- pymongo.py
+++ pymongo.py
@@ -1,1 +1,11 @@
 
+"""
+connecting with mongoDB in localhost
+"""
+
+from pymongo import MongoClient
+
+client = MongoClient("mongodb://localhost:27017/")
+db = client['spainsoccer']
+collections = db['liga_data']
+collections.insert(readings_df)
